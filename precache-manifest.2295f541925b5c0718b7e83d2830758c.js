9569e77f44b19d36a12cd28f0f9b0b73192160c3
Khaaz <Khaaz@users.noreply.github.com>
2020-06-05 20:28:06 +0000

    deploy

---
+++ precache-manifest.2295f541925b5c0718b7e83d2830758c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442f460d8693279846d0",
+    "url": "/css/app.28150258.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c02d2f455c277ca48fcaba71dfe63f21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fbabeae43fa2aeaaddd",
+    "url": "/js/about.40422146.js"
+  },
+  {
+    "revision": "442f460d8693279846d0",
+    "url": "/js/app.29c05709.js"
+  },
+  {
+    "revision": "c2e2f8b795217f04f438",
+    "url": "/js/chunk-vendors.051ed444.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
